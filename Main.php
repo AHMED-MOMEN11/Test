0a2236e21bf9c2e4568b2e96b01ee976d62852bc
AHMED-MOMEN11 <ahmedmoo101011@gmail.com>
2024-10-08 02:05:33 +0300

    first push

---
+++ Main.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test</title>
+</head>
+
+<body>
+    <?php
+    // ! first program
+    //? $name = "programer";
+    //? echo  "Hello , ".$name;
+    // ! second program
+    //? $x = 5;
+    //? $y = 10;
+    //? echo "X + Y = " . ($x + $y);
+    //? echo "<br>";
+    //? echo "X - Y = " . ($x - $y);
+    //? echo "<br>";
+    //? echo "X * Y = " . ($x * $y);
+    //! third program
+    //? $A = 1;
+    //? $B = 2;
+    //? $C = 3;
+    //? $D = 4;
+    //? $x = (($A * $B) - ($C * $D));
+    //? echo  "The Difference = " . $x;
+    ?>
+</body>
+
+</html>
